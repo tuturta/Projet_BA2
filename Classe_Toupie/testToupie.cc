6bb2a1987c3512ff2383482476b599e0618e8f31
Lea Gainon <leagainon@gmail.com>
2020-03-25 11:53:47 +0100

    Makefile

--- Classe_Toupie/testToupie.cc
+++ Classe_Toupie/testToupie.cc
@@ -6,6 +6,8 @@ int main() {
     try{
         ConeSimple cone1({0.0,0.0,0.0},{1.0,1.0,1.0},3.0,5.0,2.0);
         cout << cone1;
+        Objet_en_chute_libre obj1({0.0,0.0,0.0},{1.0,1.0,1.0},3.0);
+        cout << obj1;
         ConeSimple cone2((3),(-1),1,-2,5);
         cout << cone2;
 
