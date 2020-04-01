b75a8d05e1aef250fb397817e03f284a06d7ed62
Arthur <arthur.tabary@epfl.ch>
2020-04-01 18:33:47 +0200

    bon integrateur.cc

--- Classe_Integrateur/Integrateur.cc
+++ Classe_Integrateur/Integrateur.cc
@@ -4,17 +4,11 @@ using namespace std;
 
 
 
-void IntegrateurEulerCromer::evolue(Toupie& T, const double dt) const {
-        T.setP_point(T.getP_point() + dt*(T.fonction_f())); //PROBLEME : l'appel est fait directement à Toupie::fonction(); sans tenir compte du masquage de ObjetEnChuteLibre::fonction();
-        T.setP(T.getP() + dt*T.getP_point());
-    }
-
-void IntegrateurEulerCromer::evolue_nfois(Toupie& T, double dt, unsigned int n) const{
-    for(size_t i(1); i<=n ; ++i){
-        evolue(T,dt);
-        cout << T << endl;
-        }
+void IntegrateurEulerCromer::evolue(unique_ptr<Toupie> const& T, const double dt) const {
+        T->setP_point(T->getP_point() + dt*(T->fonction_f())); //PROBLEME : l'appel est fait directement à Toupie::fonction(); sans tenir compte du masquage de ObjetEnChuteLibre::fonction();
+        T->setP(T->getP() + dt*T->getP_point());
 }
+
 
 
 
