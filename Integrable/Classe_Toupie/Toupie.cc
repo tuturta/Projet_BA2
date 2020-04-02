9cafb85ad6d60f819088fce5be83f51221325390
Arthur <arthur.tabary@epfl.ch>
2020-04-02 11:25:05 +0200

    Changement conception/ajout integrable

---
+++ Integrable/Classe_Toupie/Toupie.cc
@@ -0,0 +1,79 @@
+#define _USE_MATH_DEFINES //Pour régler le problème de M_PI non reconnu par le compilateur sur windows
+#include <cmath>    
+#include "Toupie.h"
+#include <iostream>
+#include "../Open_GL/Classe_Dessinable/general/support_a_dessin.h"
+//#include "../Classe_Vecteur/Vecteur.h"
+//#include "../Classe_Matrice/Matrice.h"
+using namespace std;
+
+//=============================CLASSE TOUPIE===================================//
+
+// METHODES
+ostream& Toupie::affiche(ostream& sortie) const {
+    sortie << P << " # parametres" << endl;
+    sortie << P_point << " # vitesse" << endl;
+    return sortie;
+}
+
+ostream& Toupie::affiche_parametres(ostream& out) const {
+    out << "Paramètre : " << P << endl;
+    out << "Dérivée : " << P_point << endl;
+    out << "Masse volumique (kg m-3) :" << masse_volumique << endl;
+    return out;
+}
+Vecteur Toupie::getP() const {
+    return P;
+}
+Vecteur Toupie::getP_point() const {
+    return P_point;
+}
+void Toupie::setP(Vecteur const& v) {
+    P=v;
+}
+void Toupie::setP_point(Vecteur const& v){
+    P_point=v;
+}
+
+// SURCHARGES D'OPÉRATEURS 
+ostream& operator<<(std::ostream& sortie, Toupie const& toupie) {
+    return toupie.affiche(sortie);
+}
+
+
+//=============================CLASSE CONE SIMPLE===================================//
+
+//METHODES:
+
+ostream& ConeSimple::affiche_parametres(ostream& out) const {
+    Toupie::affiche_parametres(out);
+    out << "Hauteur (m) : " << hauteur << endl;
+    out << "Rayon (m) : " << rayon << endl;
+    out << "Origine (A) : " << origine << endl;
+}
+
+double ConeSimple::masse() const{ //masse calculé grace a la formule p8
+    return (1/3)*M_PI*masse_volumique*pow(rayon,2)*hauteur;
+}
+Matrice ConeSimple::matrice_inertie() const { // Matrice d'inertie calculé grace a la formule p8
+    double I1, I3;
+    I1 = masse()*((3/20)*pow(rayon,2) + (3/5)*pow(hauteur,2));
+    I3 = (3/10)*masse()*pow(rayon,2);
+    return Matrice(I1, I1, I3);
+}
+
+// Méthode virtuelle dessinable
+
+void ConeSimple::dessine() {
+    support->dessine(*this);
+} 
+
+//============================CLASSE OBJ CHUTE=====================================//
+
+void Objet_en_chute_libre::dessine() {
+    support->dessine(*this);
+}
+
+Vecteur Objet_en_chute_libre::fonction_f() const{
+        return {0,-9.81};
+}
---
+++ Integrable/Classe_Toupie/Toupie.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+//#include "../Classe_Vecteur/Vecteur.h"
+#include "../Classe_Matrice/Matrice.h"
+#include "../Open_GL/Classe_Dessinable/general/dessinable.h"
+//#include "../Classe_Integrateur/Integrateur.h"
+
+
+class Toupie : public Dessinable {
+
+   protected : 
+    Vecteur P; // Vecteur de paramètres
+    Vecteur P_point; // Dérivée temporelle des degrés de liberté
+    double masse_volumique; //Pour le moment, on le met dans la classe toupie car on ne connait pas la suite
+    Vecteur origine; //*****pour l'instant aucune autre info dessus...******
+   public :
+    Toupie (unique_ptr<SupportADessin>&& support, Vecteur const& P, Vecteur const& P_point, double masse_volumique) 
+    : Dessinable(move(support)), P(P), P_point(P_point), masse_volumique(masse_volumique), origine(P) {} // Voir plus tard pour le corps : valeurs par défaut ? Message ?
+    /*si on doit bien initialiser origine avec la pos d'origine */
+    virtual Vecteur fonction_f() const = 0; //Equation différentielles du mvt
+    
+    std::ostream& affiche(std::ostream& sortie) const;
+    
+    virtual std::ostream& affiche_parametres(std::ostream& out) const;
+
+    Vecteur getP() const;
+    Vecteur getP_point() const;
+    void setP(Vecteur const& autre);
+    void setP_point(Vecteur const& autre);
+    ///A terme, toupie sera certainement une classe virtuelle : on ne permet donc pas de la dessiner pour l'instant
+};
+std::ostream& operator<<(std::ostream& sortie,Toupie const& toupie);
+
+//===============================================================================
+
+class ConeSimple : public Toupie{
+   
+   private:
+    double hauteur;
+    double rayon;
+
+   public: 
+    ConeSimple(unique_ptr<SupportADessin> support, Vecteur const& P, Vecteur const& P_point, double masse_volumique, double hauteur, double rayon)
+    : Toupie(move(support),P, P_point, masse_volumique), hauteur(hauteur), rayon(rayon) {}
+    virtual std::ostream& affiche_parametres(std::ostream& out) const override;
+    double masse() const;
+    Matrice matrice_inertie() const; //Calcule le moment d'inertie I du cone simple
+    virtual Vecteur fonction_f() const override;
+    virtual void dessine() override;
+};
+
+
+//================================================================================
+class Objet_en_chute_libre : public Toupie {
+   public:
+    using Toupie::Toupie;
+    virtual Vecteur fonction_f() const override;
+
+    virtual void dessine() override;
+};
